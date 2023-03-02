30474e5ed5d9e6c0a6a8b9c17c688284a30cd7ff
Brad Fike <brad@fikeweb.com>
2023-03-02 11:20:19 -0600

    Basic Authentication

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/nucampsite';
+//const url = 'mongodb://localhost:27017/nucampsite';
+const url = 'mongodb+srv://bradfike:CXZx2CugBTlbyoXl@bradfikemongocluster.1cgmttv.mongodb.net/?retryWrites=true&w=majority'
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
     useFindAndModify: false,
@@ -37,6 +38,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+const auth = (req, res, next) => {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+  }
+
+  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  const user = auth[0];
+  const pass = auth[1];
+  if (user === 'admin' && pass === 'password') {
+      return next(); // authorized
+  } else {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      return next(err);
+  }
+}
+
+app.use(auth);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
