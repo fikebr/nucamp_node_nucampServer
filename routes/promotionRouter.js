ce33acb547505698bb345e43292161674aca2330
Brad Fike <brad@fikeweb.com>
2023-03-04 10:54:28 -0600

    Passport JWT

--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const promotionRouter = express.Router();
 const Promotion = require('../models/promotion');
+const authenticate = require('../authenticate');
 
 const section = 'promotion'
 
@@ -17,7 +18,7 @@ promotionRouter.route('/')
     })
     .catch(err => next(err));
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser, (req, res, next) => {
     Promotion.create(req.body)
     .then(promotion => {
         console.log('Promotion Created: ', promotion);
@@ -25,11 +26,11 @@ promotionRouter.route('/')
     })
     .catch(err => next(err));
 })
-.put((req, res) => {
+.put(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
     res.end(`PUT operation not supported on /${section}`);
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
     Promotion.deleteMany()
     .then(response => {
         res.json(response);
@@ -50,11 +51,11 @@ promotionRouter.route('/:promotionId')
     })
     .catch(err => next(err));
 })
-.post((req, res) => {
+.post(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
     res.end(`POST operation not supported on /${section}s/${req.params.promotionId}`);
 })
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
     Promotion.findByIdAndUpdate(req.params.promotionId, {
         $set: req.body
     }, {new: true})
@@ -63,7 +64,7 @@ promotionRouter.route('/:promotionId')
     })
     .catch(err => next(err));
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
     Promotion.findByIdAndDelete(req.params.promotionId)
     .then(response => {
         res.json(response);
